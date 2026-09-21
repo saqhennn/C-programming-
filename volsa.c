426e2c2ffd4762f704e16582398be2f768d819d6
Kendi <san.kendi254@gmail.com>
2026-09-21 16:44:12 +0300

    Add files via upload

---
+++ volsa.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius, height
+    double volume, surface area
+    float pi=3.142
+
+    //prompt the user
+    printf("Enter the radius:\t");
+    scanf("%f",&radius);
+
+    printf("Enter the height:\t");
+    scanf("%f",$height);
+
+    //calculate volume and surface area
+    volume= pi*radius*radius*height
+    surface area= (2*pi*radius*radius)+(2*pi*radius*height)
+
+    printf("Volume of the cylinder= %lf \n",volume);
+    printf("Surface area of the cylinder= %lf \n",surface area);
+
+    return 0
+}
