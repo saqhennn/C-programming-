9bcdc1423cc7cbae876a1b91ee3b4581b10b7156
Kendi <san.kendi254@gmail.com>
2026-09-21 16:10:48 +0300

    Add files via upload

---
+++ Userdetails.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float height(in centimeter or meter)
+    double bank balance(in kenya shillings)
+    char phone number
+
+    //prompt the user
+    printf("Enter your height(in centimeter or meter):\t");
+    scanf("%f",&height);
+
+    printf("Enter your bank balance(in kenya shillings):\t");
+    scanf("%lf,&bank balance");
+
+    printf("Enter your phone number:\t");
+    scanf("%s,&phone number")
+
+    return 0;
+}
